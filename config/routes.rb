21bcd7c2e7039b52f95ef4c7e358a74f6073781d
flares <y.manojkrishna@gmail.com>
2012-06-14 17:05:15 +0530

    adding google meta link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Mywikirev::Application.routes.draw do
 
-root :to => "users#new"
+#root :to => "users#new"
 get "log_out" => "sessions#destroy", :as => "log_out"
 get "log_in" => "sessions#new", :as => "log_in"
 get "sign_up" => "users#new", :as => "sign_up"
