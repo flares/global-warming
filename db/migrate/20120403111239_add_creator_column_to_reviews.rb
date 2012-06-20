ab9b41b335ad121126f6ea09e88aa3b5f0f17eb1
flares <y.manojkrishna@gmail.com>
2012-06-20 15:28:17 +0530

    deleted two migrate files

--- db/migrate/20120403111239_add_creator_column_to_reviews.rb
+++
@@ -1,9 +0,0 @@
-class AddCreatorColumnToReviews < ActiveRecord::Migration
-  def self.up
-    add_column :reviews, :creator, :string
-  end
-
-  def self.down
-    remove_column :reviews, :creator
-  end
-end
--- db/migrate/20120403111330_add_creator_to_reviews.rb
+++
@@ -1,9 +0,0 @@
-class AddCreatorToReviews < ActiveRecord::Migration
-  def self.up
-    add_column :reviews, :creator, :string
-  end
-
-  def self.down
-    remove_column :reviews, :creator
-  end
-end
